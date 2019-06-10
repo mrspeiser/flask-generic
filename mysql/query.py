61acfd244ace8a7d19b9983039d8ec06f9625fc9
matthew <mattrspeiser@gmail.com>
2019-06-09 22:12:49 -0600

    added some starting code for databases

---
+++ mysql/query.py
@@ -0,0 +1,10 @@
+from flask_mysqldb import MySQL
+import yaml
+
+
+
+db.yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
